3a925abee4a7bfeff896b6e445442175d9ebeeb3
DulshanSiriwardhana <dulshansiriwardhanaofficial@gmail.com>
2026-08-26 20:50:35 +0530

    Redesign: add inner padding to LocationMap card

--- src/pages/Landing/LocationMap.tsx
+++ src/pages/Landing/LocationMap.tsx
@@ -663,7 +663,9 @@ const LocationMap = () => {
                         border
                         border-white/10
                         bg-white
+                        p-2
                         shadow-[0_16px_48px_rgba(0,0,0,0.3)]
+                        sm:p-3
                         lg:flex-row
                     "
                 >
